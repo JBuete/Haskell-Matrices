72c16186bfe7f933d80a341d4f54f81f09c69b91
JBuete <jbuet2006@yahoo.com.au>
2014-05-30 23:07:09 +1000

    Initial Code
    Just the basics at the moment.

---
+++ matrices.hs
@@ -0,0 +1,39 @@
+-- Jacob Buete 2014
+--
+-- An Implementation of Matrices in Haskell
+-- A beta
+
+module Main (
+	singleton_matrix -- :: Element -> Matrix,
+	add_matrices     -- :: Matrix -> Matrix -> Matrix,
+) where
+
+data Element = Float
+	deriving Eq, Ord, Num
+
+
+type Row     = [Element]
+	deriving show
+
+
+type Matrix  = [Row]
+	deriving show
+
+singleton_matrix :: Element -> Matrix
+singleton_matrix a = [[a]]
+
+
+add_matrices :: Matrix -> Matrix -> Matrix
+add_matrices matrix_a matrix_b  = case (matrix_a, matrix_b) of 
+	([],[])     -> []
+	([],y:ys)   -> error "Your matrices can't be add together"
+	(x:xs,[])   -> error "Your matrices can't be add together"
+	(x:xs,y:ys) -> (add_rows x y): add_matrices xs ys
+					where 
+						add_rows :: Row -> Row -> Row
+						add_rows row_a row_b = case (row_a, row_b) of 
+							([],[])     -> []
+							([],y:ys)   -> error "nope"
+							(x:xs,[])   -> error "nope"
+							(x:xs,y:ys) -> (x+y):add_rows xs ys 
+
